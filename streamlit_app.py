c4e93f7ccc950d0bc64455e2fa81d211f5081a77
Jesús Soto <jesus.soto@pucp.edu.pe>
2024-08-13 06:38:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,9 +54,9 @@ with st.sidebar:
        'ANCASH', 'HUANUCO', 'ICA', 'HUANCAVELICA', 'CUSCO', 'CALLAO',
        'UCAYALI', 'TUMBES', 'SANMARTIN', 'TACNA', 'PUNO', 'PIURA',
        'PASCO', 'LORETO', 'MOQUEGUA', 'MADREDEDIOS', 'LIMA', 'LALIBERTAD',
-       'JUNIN', 'LAMBAYEQUE'].sort()
+       'JUNIN', 'LAMBAYEQUE']
     
-    selected_depart = st.selectbox('Selecciona el departamento', depart_list)
+    selected_depart = st.selectbox('Selecciona el departamento', depart_list.sort())
 
 #################################
 
